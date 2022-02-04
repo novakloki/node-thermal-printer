ef56f386548ac3b0e5e13df63afcc5a0a8f71d9a
Novak Markovic <novak.loki@gmail.com>
2022-02-04 11:20:36 +0100

    Added support for TP-8017 printer
    - Printing QR as raster image

--- node-thermal-printer.d.ts
+++ node-thermal-printer.d.ts
@@ -8,7 +8,8 @@
  */
 declare enum PrinterTypes {
   EPSON = "epson",
-  STAR = "star"
+  STAR = "star",
+  TP8017 = "TP-8017",
 }
 
 declare class ThermalPrinter {
@@ -17,7 +18,7 @@ declare class ThermalPrinter {
   /**
    * Constructor
    * @param Object config (type, interface, width, characterSet, removeSpecialCharacters, options)
-  */
+   */
   constructor(config: {
     type?: PrinterTypes;
     interface: string;
@@ -27,216 +28,218 @@ declare class ThermalPrinter {
     driver?: Object;
     removeSpecialCharacters?: boolean;
     options?: {
-      timeout?: number
+      timeout?: number;
     };
   });
 
   /**
    * Send printing buffer to printer
    * @returns Promise<String>
-  */
+   */
   execute(): Promise<String>;
 
   /**
    * Add cut
-  */
+   */
   cut(): void;
 
   /**
    * Add parital cut
-  */
+   */
   partialCut(): void;
 
   /**
    * Add beep
-  */
+   */
   beep(): void;
 
   /**
    * Get number of characters in one line
    * @returns Number
-  */
+   */
   getWidth(): number;
 
   /**
    * Get printing buffer in string
    * @returns String
-  */
+   */
   getText(): string;
 
   /**
    * Get printing buffer
    * @returns Buffer
-  */
+   */
   getBuffer(): Buffer;
 
   /**
    * Clear printing buffer
-  */
+   */
   clear(): void;
 
   /**
    * Add buffer to printing buffer
    * @param Buffer
-  */
+   */
   add(buffer: Buffer): void;
 
   /**
    * Set character set
    * @param String character set
-  */
+   */
   setCharacterSet(characterSet: String): void;
 
   /**
    * Add text
    * @param String text
-  */
+   */
   print(text: string): void;
 
   /**
    * Add text with new line
    * @param String text
-  */
+   */
   println(text: string): void;
 
   /**
    * Add vertical tab
-  */
+   */
   printVerticalTab(): void;
 
   /**
    * Set text bold
    * @param Boolean is enabled
-  */
+   */
   bold(enabled: boolean): void;
 
   /**
    * Set text undeline
    * @param Boolean is enabled
-  */
+   */
   underline(enabled: boolean): void;
 
   /**
    * Set text undeline and bold
    * @param Boolean is enabled
-  */
+   */
   underlineThick(enabled: boolean): void;
 
   /**
    * Set text upside down
-   * @param Boolean is enabled 
+   * @param Boolean is enabled
    */
   upsideDown(enabled: boolean): void;
 
   /**
    * Set text background and text color inverted
    * @param Boolean is enabled
-  */
+   */
   invert(enabled: boolean): void;
 
   /**
    * Add open cash drawer
-  */
+   */
   openCashDrawer(): void;
 
   /**
    * Align text to center
-  */
+   */
   alignCenter(): void;
 
   /**
    * Align text to left
-  */
+   */
   alignLeft(): void;
 
   /**
    * Align text to right
-  */
+   */
   alignRight(): void;
 
   /**
    * Set font type A
-  */
+   */
   setTypeFontA(): void;
 
   /**
    * Set font type B
-  */
+   */
   setTypeFontB(): void;
 
   /**
    * Set text size to normal
-  */
+   */
   setTextNormal(): void;
 
   /**
    * Set text size to double height
-  */
+   */
   setTextDoubleHeight(): void;
 
   /**
    * Set text size to double width
-  */
+   */
   setTextDoubleWidth(): void;
 
   /**
    * Set text size to double height and width
-  */
+   */
   setTextQuadArea(): void;
 
   /**
    * Add new line
-  */
+   */
   newLine(): void;
 
   /**
-    * Draw a line of characters
-  */
+   * Draw a line of characters
+   */
   drawLine(): void;
 
   /**
    * Set height and width font size
    * @param Number height
    * @param Number width
-  */
+   */
   setTextSize(height: number, width: number): void;
 
   /**
    * Add font to left side and right side
    * @param String left side text
    * @param String right side text
-  */
+   */
   leftRight(left: string, right: string): void;
 
   /**
    * Insert table of data (width split equally)
    * @param Array Array of values
-  */
+   */
   table(data: string[]): void;
 
   /**
    * Insert table of data with custom cell settings
    * @param Array Array of objects
-  */
-  tableCustom(data: {
-    text: string;
-    align?: "CENTER" | "RIGHT" | "LEFT";
-    width?: number;
-    cols?: number;
-    bold?: boolean;
-  }[]): void;
+   */
+  tableCustom(
+    data: {
+      text: string;
+      align?: "CENTER" | "RIGHT" | "LEFT";
+      width?: number;
+      cols?: number;
+      bold?: boolean;
+    }[]
+  ): void;
 
   /**
    * Check if printer is connected
    * @returns Promise<boolean>
-  */
+   */
   isPrinterConnected(): Promise<boolean>;
 
   /**
    * Print QR code
    * @param String QR data
    * @param Object Settings (cellSize, correction, model)
-  */
+   */
   printQR(
     str: string,
     settings?: {
@@ -251,7 +254,7 @@ declare class ThermalPrinter {
    * @param String barcode data
    * @param Number type of barcode
    * @param Object barcode settings (hriPos, hriFont, width, height)
-  */
+   */
   printBarcode(
     data: string,
     type?: number,
@@ -267,7 +270,7 @@ declare class ThermalPrinter {
    * Add maxiCode barcode
    * @param String barcode data
    * @param Object barcode settings (mode)
-  */
+   */
   maxiCode(
     data: string,
     settings?: {
@@ -279,7 +282,7 @@ declare class ThermalPrinter {
    * Add code128 barcode
    * @param String barcode data
    * @param Object barcode settings (width, height, text)
-  */
+   */
   code128(
     data: string,
     settings?: {
@@ -293,7 +296,7 @@ declare class ThermalPrinter {
    * Add pdf417 barcode
    * @param String barcode data
    * @param Object settings (rowHeight, width, correction, truncated, columns)
-  */
+   */
   pdf417(
     data: string,
     settings?: {
@@ -309,27 +312,27 @@ declare class ThermalPrinter {
    * Add image
    * @param String file path
    * @returns Promise<Buffer> image buffer
-  */
+   */
   printImage(image: string): Promise<Buffer>;
 
   /**
    * Add image buffer
    * @param Buffer image buffer
    * @returns Promise<Buffer> printer image buffer
-  */
+   */
   printImageBuffer(buffer: Buffer): Promise<Buffer>;
 
   /**
    * Send buffer to printer
    * @param Buffer
    * @returns Promise<String>
-  */
+   */
   raw(text: Buffer): Promise<String>;
 
   /**
    * Manually set the printer driver
    * @param Object driver the printer driver
-  */
+   */
   setPrinterDriver(driver: Object): void;
 
   /**
@@ -339,8 +342,4 @@ declare class ThermalPrinter {
   append(content: string | Buffer): void;
 }
 
-
-export {
-  ThermalPrinter as printer,
-  PrinterTypes as types
-};
+export { ThermalPrinter as printer, PrinterTypes as types };
